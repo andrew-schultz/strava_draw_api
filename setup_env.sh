bb661e6bd69d3e491391bcd77c615a5e906c1baa
Andy Oh-Schultz <as173171@gmail.com>
2025-03-11 12:34:22 -0400

    fix gitlab-ci generated .env.prod

--- setup_env.sh
+++ setup_env.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-echo DEBUG=0 >> .env
-echo SQL_ENGINE=django.db.backends.postgresql >> .env
-echo DATABASE=postgres >> .env
+echo DEBUG=0 >> .env.prod
+echo SQL_ENGINE=django.db.backends.postgresql >> .env.prod
+echo DATABASE=postgres >> .env.prod
 
-echo SECRET_KEY=$SECRET_KEY >> .env
-echo SQL_DATABASE=$SQL_DATABASE >> .env
-echo SQL_USER=$SQL_USER >> .env
-echo SQL_PASSWORD=$SQL_PASSWORD >> .env
-echo SQL_HOST=$SQL_HOST >> .env
-echo SQL_PORT=$SQL_PORT >> .env
+echo SECRET_KEY=$SECRET_KEY >> .env.prod
+echo SQL_DATABASE=$SQL_DATABASE >> .env.prod
+echo SQL_USER=$SQL_USER >> .env.prod
+echo SQL_PASSWORD=$SQL_PASSWORD >> .env.prod
+echo SQL_HOST=$SQL_HOST >> .env.prod
+echo SQL_PORT=$SQL_PORT >> .env.prod
+echo SQL_SSL=$SQL_SSL >> .env.prod
+
+echo DJANGO_ALLOWED_HOSTS=$DJANGO_ALLOWED_HOSTS >> .env.prod
+echo STRAVA_CLIENT_ID=$STRAVA_CLIENT_ID >> .env.prod
+echo STRAVA_CLIENT_SECRET=$STRAVA_CLIENT_SECRET>> .env.prod
+echo CORS_RULE=$CORS_RULE >> .env.prod
+echo SIG_KEY=$SIG_KEY >> .env.prod
+echo JWT_SECRET=$JWT_SECRET >> .env.prod
