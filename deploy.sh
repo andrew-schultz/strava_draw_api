451bbe793d0e62da42bd7a1bd9e4004651ef5bce
Andy Oh-Schultz <as173171@gmail.com>
2025-03-14 19:43:05 -0400

    add the start command back to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
-#   cd /app
-#   export $(cat .env.prod | xargs)
-#   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-#   docker pull $IMAGE:web
-#   docker pull $IMAGE:nginx
-#   docker pull $IMAGE:certbot
-#   docker compose -f docker-compose.yml up -d
-# ENDSSH
-
 ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /app
   export $(cat .env.prod | xargs)
@@ -17,4 +7,5 @@ ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   docker pull $IMAGE:web
   docker pull $IMAGE:nginx
   docker pull $IMAGE:certbot
+  docker compose -f docker-compose.yml up -d
 ENDSSH
