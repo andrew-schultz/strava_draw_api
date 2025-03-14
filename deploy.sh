6c3480e2de3910bac50025fa6a230e58e8c351ae
Andy Oh-Schultz <as173171@gmail.com>
2025-03-14 19:31:29 -0400

    temp remove docker-compose up in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
+#   cd /app
+#   export $(cat .env.prod | xargs)
+#   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+#   docker pull $IMAGE:web
+#   docker pull $IMAGE:nginx
+#   docker pull $IMAGE:certbot
+#   docker compose -f docker-compose.yml up -d
+# ENDSSH
+
 ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /app
   export $(cat .env.prod | xargs)
@@ -7,5 +17,4 @@ ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   docker pull $IMAGE:web
   docker pull $IMAGE:nginx
   docker pull $IMAGE:certbot
-  docker compose -f docker-compose.yml up -d
 ENDSSH
--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(routeviewer.com www.routeviewer.com)
 rsa_key_size=4096
 data_path="./data/certbot_build"
 email="as173171@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -73,7 +73,7 @@ docker-compose -f docker-compose.yml run --rm --entrypoint "\
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal" certbot
+    --force-renewal" certbot -v
 echo
 
 echo "### Reloading nginx ..."
