5c6fb3ccaf1b43ac03eadb272fc5b6e015ef174b
Andy Oh-Schultz <as173171@gmail.com>
2025-03-16 00:50:24 -0400

    think we're getting somewhere again

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,19 @@ ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   export $(cat .env.prod | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $IMAGE:web
-  docker pull $IMAGE:nginx
   docker pull $IMAGE:certbot
   docker compose -f docker-compose.yml up -d
 ENDSSH
+
+
+# #!/bin/sh
+
+# ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
+#   cd /app
+#   export $(cat .env.prod | xargs)
+#   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+#   docker pull $IMAGE:web
+#   docker pull $IMAGE:nginx
+#   docker pull $IMAGE:certbot
+#   docker compose -f docker-compose.yml up -d
+# ENDSSH
